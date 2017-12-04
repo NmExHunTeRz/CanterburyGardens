26f77556ce0d6e9ff4d9add498abf0693e299d57
= <=>
2017-12-04 17:18:06 +0000

    Base

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    /**
+     * Our initial index function that gets hit on initial page load
+     */
+    public function index()
+    {
+        dd("hit");
+    }
+}
