d07af2262f4f930d069c1eb8139656e9ea073fde
S.Dan <barddan@gmail.com>
2017-12-05 22:27:13 +0000

    Removed Weather Widget

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,8 +15,6 @@
             Huy's map will go here
           </div>
           <div class="col-sm-4 weather">
-            <div id="openweathermap-widget-15"></div>
-            <script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = []; window.myWidgetParam.push({ id: 15, cityid: '2653877', appid: '207c0e71469a721950916a9720d0ae71', units: 'metric', containerid: 'openweathermap-widget-15', }); (function () { var script = document.createElement('script'); script.async = true; script.charset = "utf-8"; script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js"; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s); })();</script>
           </div>
         </div>
 @endsection
