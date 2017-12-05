5ed4d9f61a53725bfe9f9c6bcd6a7647f698444d
S.Dan <barddan@gmail.com>
2017-12-05 21:41:33 +0000

    Set new template for app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-theme.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://bootswatch.com/3/cosmo/bootstrap.css">
 </head>
 <body>
@@ -70,10 +71,73 @@
             </div>
         </div>
     </nav>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Cooksey Farms</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <li class="active">
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="https://www.metoffice.gov.uk/public/weather/forecast/u10g8x4vg">Weather</a>
+          </li>
+
+        </ul>
+        <ul class="nav nav-sidebar">
+          <li>
+            <a href="./locations/gh1.html">Greenhouse 1</a>
+          </li>
+          <li>
+            <a href="./locations/gh2.html">Greenhouse 2</a>
+          </li>
+          <li>
+            <a href="./locations/gh3.html">Greenhouse 3</a>
+          </li>
+          <li>
+            <a href="./locations/outdoors.html">Outdoor Plots</a>
+          </li>
+          <li>
+            <a href="./locations/muck.html">Muck Heap</a>
+          </li>
+          <li>
+            <a href="./locations/store.html">Store Room</a>
+          </li>
+        </ul>
+        <ul class="nav nav-sidebar">
+          <li>
+            <a href="./locations/solar.html">Solar Plant</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     @yield('content')
+      </div>    
+    </div>
+</div>
 </div>
 
 <!-- Scripts -->
+<footer class="footer">
+        <div class="container">
+            <p class="text-muted">Cooksey Farms</p>
+        </div>
+    </footer>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script type="text/javascript">window.onload = onLoad() </script>
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
