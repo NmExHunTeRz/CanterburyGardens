10116e38c57778d200b5314ca6ef7702c3ffff11
Deniz <denizaygun@hotmail.co.uk>
2017-12-05 19:08:21 +0000

    Various changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Bootstrap theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://bootswatch.com/3/cosmo/bootstrap.css">
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
@@ -51,7 +51,7 @@
                     </select>
                 </div>
                 <div class="col-sm-2">
-                    <button class="btn btn-info" style="margin-top: 24px; padding: 6px 40px" id="view_device">View</button>
+                    <button class="btn btn-success" id="view_device">View</button>
                 </div>
                 <div class="col-sm-12">
                     <canvas id="myChart" width="400" height="400"></canvas>
