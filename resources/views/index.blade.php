ce819849d11eedd6e68befa98351914fb190a9ff
Huy Nguyen <kila.huy@gmail.com>
2017-12-06 12:33:47 +0000

    add rest of optimal data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,12 +6,7 @@
 		console.log(sites);
 		window.devices = {!! json_encode($devices) !!};
 		console.log(devices);
-
-		$.each(window.sites, function(siteIndex, site) {
-
-		});
 	</script>
-
 
 	<h1 class="page-header">Dashboard</h1>
 	<div class='container dash-container'>
@@ -45,14 +40,4 @@
 			</div>
 		</div>
 	</div>
-	<div class="container">
-  <h2>Simple Collapsible</h2>
-  <p>Click on the button to toggle between showing and hiding content.</p>
-  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
-  <div id="demo" class="collapse">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  </div>
-</div>
 @endsection
