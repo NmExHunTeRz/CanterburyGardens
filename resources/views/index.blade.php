973b781a0b8a8755e05c97bcf9fb094ed42f76e9
S.Dan <barddan@gmail.com>
2017-12-06 14:53:38 +0000

    Reapplied the formatting Huy removed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,16 @@
 		console.log(devices);
 	</script>
 
-	<h1 class="page-header">Dashboard</h1>
-	<div class='container dash-container'>
+<div class='container dash-container'>
 		<div class='row'>
-			<div class='col-xs-12 col-md-12 col-lg-12 dash-notices'>
+		</br></br>
+		<h1 class="page-header">Dashboard</h1>
+			<div class='col-xs-12 col-md-6 col-lg-6 dash-notices'>
 				<h3>Notifications for <span id="date"></span></h3>
 				<ul id='notifications'></ul>
 			</div>
+		</div>
+		<div class="row">
 			<div class='col-xs-12 col-md-12 col-lg-12 dash-status'>
 				<h3>Device statuses</h3>
 				<div id='statuses'>
@@ -41,5 +44,89 @@
 				</div>
 			</div>
 		</div>
+		<div class="container">
+    <div class="panel panel-default">
+        <div class="panel-heading">Devices</div>
+        <div class="panel-body">
+            <div class="form-group">
+                <div class="col-sm-10">
+                    <label for="devices">Select a device:</label>
+                    <select class="form-control" id="devices">
+                        @foreach ($devices as $key => $device)
+                            <option value="{{$key}}">{{$device->name}} ({{$device->id}})</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-sm-2">
+                    <button class="btn btn-success" id="view_device">View</button>
+                </div>
+                <div class="col-sm-12">
+                    <canvas id="myChart" width="400" height="400"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 	</div>
+
+	<script>
+    $(document).ready(function() {
+        var chart = null;
+
+        $("#view_device").click(function() {
+            if (chart) // If we have an existing chart then we need to destroy it before generating a new one
+                chart.destroy();
+
+            var id = $("#devices option:selected").val();
+
+            var encoded_data = {!! json_encode($devices) !!};
+
+            console.log(encoded_data);
+
+            generateLineGraph(encoded_data[id]['readings'], encoded_data[id]['timestamps']);
+        });
+
+        function generateLineGraph (data, labels) {
+            $("#myChart").html('');
+            var ctx = document.getElementById('myChart').getContext('2d');
+
+            chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: '# reading',
+                        data: data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    });
+</script>
 @endsection
