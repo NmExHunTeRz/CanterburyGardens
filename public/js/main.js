de033738368c598c82bd485ef46844880cf17981
Deniz <denizaygun@hotmail.co.uk>
2017-12-06 16:10:33 +0000

    Minor changes

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-function onLoad() {
-    n = new Date();
-    y = n.getFullYear();
-    m = n.getMonth() + 1;
-    d = n.getDate();
-    document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
-}
