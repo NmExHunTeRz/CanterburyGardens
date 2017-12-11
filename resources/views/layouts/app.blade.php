0aa0a38c1ffbd359d551c595efbc5c797e8fea8e
Huy Nguyen <kila.huy@gmail.com>
2017-12-11 15:40:57 +0000

    Add icons creditation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,6 +79,7 @@
 	<footer class="footer">
 		<div class="container">
 			<p class="text-muted">Canterbury Gardens Ltd - {{ date("Y") }}</p>
+			<div id='credits'>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 		</div>
 	</footer>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
